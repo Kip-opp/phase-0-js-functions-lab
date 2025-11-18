06ef5cd3d105f94ce642fbae88cbd25b0f46318f
denis@kyp <kypvega@gmail.com>
2025-11-18 16:10:33 +0300

    Implement budgeting functions for lab

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-
-
-
-
-
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  // Remove case sensitivity and non-letter characters for robustness (if not required, remove replace/regex)
+  const normalized = word.toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
+  return normalized === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
